e2d8107369ba2704ec163bea50f85b644f454d26
annaokragly <anna.okragly@gmail.com>
2019-03-01 14:56:55 +0100

    Adding redux to the app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App';
+import { Provider} from 'react-redux';
+import{ createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import registerServiceWorker from './registerServiceWorker';
+import { requestRobots, searchRobots } from './reducers';
 import 'tachyons';
-import { robots } from './robots';
+import App from './containers/App';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const logger = createLogger();
+
+const rootReducer = combineReducers({ requestRobots, searchRobots })
+
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+
+ReactDOM.render(
+                <Provider store={store}>
+                    <App />
+                </Provider>, document.getElementById('root'));
 registerServiceWorker();
